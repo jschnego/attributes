a6aa51386dc1245a721749ee3ad596ad7efd9e32
Jan Schnegotzki <janschnegotzki@hotmail.de>
2020-09-15 22:57:07 +0800

    Create barebones app and add necessary dependencies to pom.xml

---
+++ src/test/java/com/js/AppTest.java
@@ -0,0 +1,17 @@
+package com.js;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
